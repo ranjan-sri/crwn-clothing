59dc3a716c4cafd4c73b7e22830aa683bff7cfbf
sriranga-nagi <62817358+sriranga-nagi@users.noreply.github.com>
2022-02-14 20:10:13 -0800

    update our menuItem to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./App.css";
+import {Switch, Route} from 'react-router-dom'
 import  Homepage from './pages/homepage/homepage.component.jsx'
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+      </Switch>
     </div>
   ) 
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import MenuItem from '../menu-item/menu-item.component'
+
 
 import './directory.styles.scss'
+
+import MenuItem from '../menu-item/menu-item.component'
+
+
 
 class Directory extends React.Component {
     constructor(){
@@ -50,8 +54,8 @@ class Directory extends React.Component {
         return(
         <div className="directory-menu">
             {
-           this.state.sections.map(({title, id, imageUrl, size}) => (
-              <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} /> 
+           this.state.sections.map(({id, ...otherSectionProps}) => (
+              <MenuItem key={id} {...otherSectionProps}  /> 
            ))
         }
         </div>    
