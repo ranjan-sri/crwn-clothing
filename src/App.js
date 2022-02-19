81db54e605de1eb6c015821d71c9478b95550241
sriranga-nagi <62817358+sriranga-nagi@users.noreply.github.com>
2022-02-18 19:37:07 -0800

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import  Homepage from './pages/homepage/homepage.component.jsx'
 import  shopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 
 class App extends React.Component {
@@ -21,10 +21,31 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth =  auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
-
-      console.log(user)
+    this.unsubscribeFromAuth =  auth.onAuthStateChanged(async userAuth => {
+      
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        console.log(userRef)
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, () =>{
+            console.log(this.state)
+          })     
+        })
+       
+        
+      
+      }
+     else {
+      this.setState({currentUser: userAuth})
+     }
+     
+      
+      
     })
   }
 
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './sign-in.styles.scss'
 import FormInput from '../../components/form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
-import { signInWithGoogle} from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle} from '../../firebase/firebase.utils'
 class SignIn extends React.Component {
     constructor(props) {
         super(props)
@@ -14,10 +14,21 @@ class SignIn extends React.Component {
        } 
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+        
+        const {email, password} = this.state
 
-        this.setState({email:'', password: ''})
+        try {
+           await auth.signInWithEmailAndPassword(email, password)
+           this.setState(
+               { email: '',
+                 password: ''
+                }
+                 )
+        } catch (error){
+            console.log(error)
+        }
     }
     
     handleChange = event => {
