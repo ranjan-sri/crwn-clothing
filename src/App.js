60efd679428d2bef16610c790610d505db9a6cbc
sriranga-nagi <62817358+sriranga-nagi@users.noreply.github.com>
2022-02-22 20:45:17 -0800

    added component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { setCurrentUser } from './redux/user/user.actions';
 
 //Import components
 import  Homepage from './pages/homepage/homepage.component.jsx'
-import  shopPage from './pages/shop/shop.component.jsx'
+import  ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
@@ -55,7 +55,7 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={Homepage}/>
-          <Route exact path='/shop' component={shopPage}/>
+          <Route path='/shop' component={ShopPage}/>
           <Route exact path='/checkout' component={CheckoutPage}/>
           <Route exact 
                  path='/signin' 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import {PersistGate} from 'redux-persist/integration/react'
 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from './redux/store'
+import {store, persistor} from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App/>
+      <PersistGate persistor={persistor}>
+         <App/>
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
